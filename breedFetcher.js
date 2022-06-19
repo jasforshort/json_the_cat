334be05eb85c2a845fec8531f56f7997e2265168
Jas Mann <jaforshort@hotmail.ca>
2022-06-19 23:23:37 +0000

    Refactored application into a function and added tests with mocha & chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,20 @@
 const request = require('request');
 
-const breedName = process.argv[2];
+const fetchBreedDescription = function(breed, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    if (error) {
+      callback(error);
+    } else {
+      // convert the JSON string in an object
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        error = "The requested breed was not found";
+        callback(error);
+      } else {
+        callback(error, data[0].description);
+      }
+    }
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  // convert the JSON string in an object
-  const data = JSON.parse(body);
-
-  if (data.length === 0) {
-    console.log("The requested breed was not found");
-  } else {
-    console.log(data[0].description);
-  }
-});
+module.exports = { fetchBreedDescription };
